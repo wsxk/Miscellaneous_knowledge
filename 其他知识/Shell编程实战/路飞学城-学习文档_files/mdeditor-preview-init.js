74d82d0e97d0a29079823935d62dfe1429cee9bd
wsxk <79676829+wsxk@users.noreply.github.com>
2021-08-31 11:30:59 +0800

    Delete 其他知识 directory

--- 其他知识/Shell编程实战/路飞学城-学习文档_files/mdeditor-preview-init.js
+++
@@ -1,7 +0,0 @@
-(function () {
-    $('.mdeditor-preview-area').each(function () {
-        var area_id = $(this).attr('id');
-        var config = JSON.parse($(this).find('textarea').attr('mdeditor-preview-config'));
-        editormd.markdownToHTML(area_id,config);
-    });
-})();
